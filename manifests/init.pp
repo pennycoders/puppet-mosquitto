57b2d1e2b56229c2706358c72ebd0a3427fa0e72
Alex Pica <dtk077@gmail.com>
2014-02-23 11:36:30 +0200

    Move repo in a separate class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,76 @@
 # Class: mosquitto
 #
+# Author: Alex P.
 # This module manages mosquitto
 #
-# Parameters: 
-# [*manage_repo*] - Ensures the mosquitto repo is added, automatically
-# [*conf_template*] - The path to the template file,
+# Parameters:
+#
+# [*manage_repo*]
+# Whether the mosquitto repo should be added or not, defaults to true
+# # [*manage_service*]
+#  Whether to manage the mosquitto service or not
+#
+#
+# [*service_name*]
+#  Service name to manage
+#
+# [*conf_template*]
+#  The path to a puppet template file to be rendered as the main mosquitto configuration file
+#  defaults to mosquitto/mqtt.conf.erb
+#
+# [*manage_service*]
+#  Whether to manage the mosquitto service or not
+#
+# [*conf_template*]
+# The path to a template file, defaults to mosquitto/mqtt.conf.erb
+#
+# [*allow_anonymous*]
+# Boolean value that determines whether clients that connect without providing a username are allowed to connect. If set to false
+# then another means of connection should be created to control authenticated client access. Defaults to true.
+#
+# Reloaded on reload signal.
+#
+# [*auth_opt_qos*]
+# The path to a template file, defaults to mosquitto/mqtt.conf.erb
+#
+# [*allow_duplicate_messages*]
+# If a client is subscribed to multiple subscriptions that overlap, e.g. foo/# and foo/+/baz , then MQTT expects that when the
+# broker receives a message on a topic that matches both subscriptions, such as foo/bar/baz, then the client should only receive the
+# message once.
+#
+# Mosquitto keeps track of which clients a message has been sent to in order to meet this requirement. This option allows this
+# behaviour to be disabled, which may be useful if you have a large number of clients subscribed to the same set of topics and want
+# to minimise memory usage.
+#
+# It can be safely set to true if you know in advance that your clients will never have overlapping subscriptions, otherwise your
+# clients must be able to correctly deal with duplicate messages even when then have QoS=2.
+#
+# Defaults to true.
+#
+# Reloaded on reload signal.
+#
+# [*conf_template*]
+# The path to a template file, defaults to mosquitto/mqtt.conf.erb
+#
+#
+#
 # Actions:
 #
 # Requires: see Modulefile
 #
 # Sample Usage:
+# class {'mosquitto':}
 #
 class mosquitto (
   $manage_repo                  = true,
+  $manage_service               = true,
+  $service_name                 = 'mosquitto',
   $conf_template                = 'mosquitto/mqtt.conf.erb',
-  $global_opts                  = undef,
   $allow_anonymous              = true,
   $auth_opt_qos                 = true,
+  $autosave_interval            = '1800',
+  $autosave_on_changes          = true,
+  $allow_duplicate_messages     = true,
   $connection_messages          = true,
   $max_inflight_messages        = '0',
   $max_queued_messages          = '0',
@@ -28,13 +82,7 @@ class mosquitto (
   $listen_port                  = '2000',
   $max_connections              = '-1') {
   if $manage_repo == true {
-    yum::managed_yumrepo { 'mosquitto':
-      descr    => "Mosquitto CentOS 6 Repository",
-      baseurl  => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/",
-      enabled  => 1,
-      gpgcheck => 1,
-      gpgkey   => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/repodata/repomd.xml.key"
-    }
+    include mosquitto::repo::mosquitto
   }
 
   package { [
@@ -46,13 +94,21 @@ class mosquitto (
   }
 
   if $conf_template != undef {
+    if $manage_service == true {
+      service { $service_name: ensure => running }
+      $notify_service = Service[$service_name]
+    } else {
+      $notify_service = undef
+    }
+
     file { '/etc/mosquitto/conf.d/mqtt.conf':
       ensure  => file,
       content => template($conf_template),
+      notify  => $notify_service,
       require => [
         Package['mosquitto'],
         Package['libmosquitto-devel'],
-        Package['libmosquittopp-devel']]
+        Package['libmosquittopp-devel']],
     }
   }
 }
---
+++ manifests/repo/mosquitto.pp
@@ -0,0 +1,13 @@
+# Class mosquitto::repo::mosquitto
+# Author: Alex P.
+
+
+class mosquitto::repo::mosquitto {
+  yum::managed_yumrepo { 'mosquitto':
+    descr    => "Mosquitto CentOS 6 Repository",
+    baseurl  => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/",
+    enabled  => 1,
+    gpgcheck => 1,
+    gpgkey   => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/repodata/repomd.xml.key"
+  }
+}
