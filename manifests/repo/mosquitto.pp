a0defe5d7cfad5ffa81b2df6a73b1e8b7a7ebce1
Kris Buytaert <sdog@inuits.be>
2014-03-21 22:07:04 +0100

    Lint

--- manifests/repo/mosquitto.pp
+++ manifests/repo/mosquitto.pp
@@ -4,10 +4,10 @@
 
 class mosquitto::repo::mosquitto {
   yumrepo { 'mosquitto':
-    descr    => "Mosquitto CentOS 6 Repository",
-    baseurl  => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/",
+    descr    => 'Mosquitto CentOS 6 Repository',
+    baseurl  => 'http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/',
     enabled  => 1,
     gpgcheck => 1,
-    gpgkey   => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/repodata/repomd.xml.key"
+    gpgkey   => 'http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/repodata/repomd.xml.key'
   }
 }
